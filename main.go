847a697e7f018e1ef6abffa572bca3a9d30acc89
matthias glenk <matthias.glenk@aracom.de>
2025-12-10 12:00:07 +0100

    removed comments

--- main.go
+++ main.go
@@ -8,14 +8,6 @@ import (
 	"github.com/gorgoroth31/chatty/models"
 )
 
-// commands
-// join 192.168.0.75
-// start => startet chatroom und gibt IP zurück unter der der Raum erreichbar ist
-
-// wenn nutzer nachricht sendet, wird die Nachricht an den Host geschickt, der schickt dann die Nachricht an alle weiter
-// soll ich jeden gleich behandeln, also auch dass der Host die Nachricht an sich sendet und dann den send an den Rest triggert?
-// soll ich für den host einen webserver aufmachen, an den dann die Nachrichten geschickt werden? => Glaube ja
-
 func main() {
 	user := models.GetUserInstance()
 	user.IpAddr = GetLocalIP().String()
@@ -24,7 +16,6 @@ func main() {
 
 	if os.Args[1] == "start" {
 		startServer()
-		// joinMySelf
 		joinSignalR(GetLocalIP().String(), p)
 
 	} else if os.Args[1] == "join" {
